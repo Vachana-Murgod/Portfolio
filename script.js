16418506f41cd8336a6af48e7ddf60dea9d945bb
vachana <murgodvachana@gmail.com>
2024-02-26 21:27:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a");
+
+menuBtn.addEventListener("click",  () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener("click", () => {
+    menuBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  });
+});
+
+
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll(".reveal");
+
+  for(var i = 0; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Form submission
+  const contactForm = document.querySelector('.contact-form');
+
+  contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value; 
+
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message); 
+  });
+});
